166197869af8fab5c61efca2a23849b2eec72fd7
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-11 11:05:33 -0700

    Psuedo code

---
+++ assets/javaScript/script.js
@@ -0,0 +1,38 @@
+var myApi = "fd0962dc486d115b23abc1e5d7586b6b"
+
+fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API Key}')
+    .then(response => response.json())
+    .then(data => console.log(data));
+// Base url for fetching weather.
+
+// Format the Url for specific assignment criteria.
+    // When I search a city, I get current and future weather (5 days) displayed to appropitate locations.
+    // City name, the date, icon representation of weather conditions, temp, humidity, wind speed, and uv index.
+
+        // Create function that uses user input to search for desired city.
+            // Event listener for search function.
+            // Render the reults to appropriate locations
+
+
+
+// Presented with color depending on weather conditions, good, ok, suck.
+    // Change Class depending on conditions.
+        // Create and attach class based of conditions.
+
+
+// Future weather must contain icon of weather conditions, the temp, wind speed, and humidity.
+    // 5 day forecast specification.
+        // Render to approptiate locations.
+
+
+
+// Should be able to select previously selected city and retreive the information.
+    // store search to local storage.
+    // retrieve from local storage.
+
+
+
+
+
+
+
