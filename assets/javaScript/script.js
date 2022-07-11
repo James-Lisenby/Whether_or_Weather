3734d2ca6530aaeda2bf70c9e28cedc8463afd64
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-11 12:48:40 -0700

    Created more JS code

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -6,14 +6,23 @@ fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclu
 // Base url for fetching weather.
 
 // Format the Url for specific assignment criteria.
-    // When I search a city, I get current and future weather (5 days) displayed to appropitate locations.
-    // City name, the date, icon representation of weather conditions, temp, humidity, wind speed, and uv index.
+// When I search a city, I get current and future weather (5 days) displayed to appropitate locations.
+// City name, the date, icon representation of weather conditions, temp, humidity, wind speed, and uv index.
 
-        // Create function that uses user input to search for desired city.
-            // Event listener for search function.
-            // Render the reults to appropriate locations
+// Create function that uses user input to search for desired city.
+// Event listener for search function.
+// Render the reults to appropriate locations
+
+var cityInput = document.getElementById("selectedCity").value;
+var savedCitys = document.getElementById("savedSearch");
+var searchButton = document.getElementById("saveBtn");
+
+function getCity() {
 
 
+}
+
+searchButton.addEventListener("click", getCity);
 
 // Presented with color depending on weather conditions, good, ok, suck.
     // Change Class depending on conditions.
