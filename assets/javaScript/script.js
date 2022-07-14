42d51606856a5e73bdb9f770710dab4a6ad58f6e
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-14 13:36:27 -0700

    ugh

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,1 +1,3 @@
-var myApi = "4637c879cb34b889eb8beec14aa12735"
+var myApi = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=4637c879cb34b889eb8beec14aa12735'
+
+console.log(myApi);
