0bbdcf95156e231659797b293c5bc094850cedb4
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-25 14:05:41 -0700

    api will not work...whyyy

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,3 +1,25 @@
-var myApi = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=4637c879cb34b889eb8beec14aa12735'
-
+var myApi = 'a783470c8e0e839974c5046a1b0cdf57'
 console.log(myApi);
+// Api for Open Weather source.
+
+var searchedList = document.getElementById('searchedList');
+
+function getApi() {
+
+    var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?&appid=14db4ea9ed26c7d98a29d7942a363b4a';
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var listItem = document.createElement('li');
+                listItem.textContent = data[i].html.url;
+                searchedList.appendchild(listItem);
+            }
+        });
+};
+
+var searchBtn = document.getElementById('find');
+searchBtn.addEventListener('click', getApi);  
